9de9dd3f8c0bd01f5b52ef96733a498fc83cf763
abeka-buckaroo <122269588+abeka-buckaroo@users.noreply.github.com>
2023-01-18 10:12:30 +0100

    Credit Click Validation

--- packages/buckaroo/buckaroo-wrapper/src/Validate/CreditClick.php
+++ packages/buckaroo/buckaroo-wrapper/src/Validate/CreditClick.php
@@ -7,4 +7,37 @@ use Illuminate\Support\Facades\Validator;
 class CreditClick
 {
 
+    /**
+     * @param array $data
+     */
+
+    public static function pay(array $data){
+        //Validate PayRecurrent
+
+        $validator = Validator::make($data, [
+            'amountDebit' => 'required|numeric',
+            'invoice' => 'string',
+            'email' => 'required|email',
+            'customer.firstName' => 'required|string',
+            'customer.lastName' => 'required|string'
+        ]);
+        return $validator;
+    }
+
+    /**
+     * @param array $data
+     */
+    public static function refund(array $data){
+        //Validate PayRecurrent
+        
+        $validator = Validator::make($data, [
+            'amountCredit' => 'required|numeric',
+            'invoice'       => 'string',
+            'description'   => 'required|string',
+            'originalTransactionKey' => 'required|string',
+            'serviceParameters.reason' => 'required|string'
+        ]);
+
+        return $validator;
+    }
 }
